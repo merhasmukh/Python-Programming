b2937ea39b33c63f1697ad1af41b9d8481b1a43a
mer hasmukh <55354471+mhgn@users.noreply.github.com>
2021-04-30 22:28:51 +0530

    Add files via upload

---
+++ Range Function.py
@@ -0,0 +1,42 @@
+
+r1=range(10)
+print(typ
+e(r1))
+print(r1) # range(0,10) will be output
+
+for i in
+
+#printing Numbers Using Range
+
+for i in range(10):
+	print(i)
+	
+	
+	
+	
+	
+#Print Numbers With Start Point And End Points
+
+#It Start From 10 and Goes Up-To 49
+
+for i in range(10,50):
+	print(i)	
+	
+	
+	
+#Range Function With Three Parameters/Arguments
+#Print Numbers From Start Point To End Point With Some Steps
+
+for i in range(5,50,5):
+	print(i)
+	
+	
+	
+
+#Iteration Using Range Function
+
+
+l1=[1,2,3,4,5,6,7,45,12,23]
+
+for i in range(len(l1)):
+	print(l1[i])	
