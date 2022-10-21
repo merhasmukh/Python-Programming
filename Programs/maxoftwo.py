f36328bbf096731086c4e09cf8220ad702d3896a
mhgn <mhgn0001@gmail.com>
2022-10-21 20:48:43 +0530

    Update Python programs with assignments

--- Programs/maxoftwo.py
+++
@@ -1,8 +0,0 @@
-num1=int(input("Enter First Number: "))
-num2=int(input("Enter Second Number: "))
-
-
-if num1 > num2:
-	print("Maximun number is {}".format(num1))
-else:
-	print("Maximun number is {}".format(num2))	
---
+++ python_crash_course/README.md
@@ -0,0 +1,7 @@
+# python-programming
+
+## Follow This Link:
+
+### <a href="https://github.com/mhgn/Full_Stack_Data_Science/tree/main/python">https://github.com/mhgn/Full_Stack_Data_Science/tree/main/python</a>
+
+
